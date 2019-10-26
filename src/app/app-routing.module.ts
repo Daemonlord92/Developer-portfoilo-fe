5f576029e766d06824e1d41f15b3d00f2597d53a
Matthew Martin <zhintateo19924559@gmail.com>
2019-10-26 13:31:26 -0700

    restart on the Developer portfoilo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: 'home', component: HomepageComponent },
+{ path: 'projects', component: ProjectsComponent }, 
+{ path: 'contact', component: ContactComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: 'about-me', component: AboutMeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BlogComponent,
+    ProjectsComponent,
+    ContactComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
