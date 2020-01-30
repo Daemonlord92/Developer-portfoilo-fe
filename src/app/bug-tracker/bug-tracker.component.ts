a3813dbcb9c50b94cf8b22937fc237e42c545b71
Matthew Martin <zhintateo19924559@gmail.com>
2020-01-29 21:02:47 -0600

    Github Links working in bug tracker and working on responsive design

--- src/app/bug-tracker/bug-tracker.component.ts
+++ src/app/bug-tracker/bug-tracker.component.ts
@@ -9,29 +9,32 @@ import { BugTracker } from './bug-tracker';
 })
 export class BugTrackerComponent implements OnInit {
 
-	bugOne: BugTracker = new BugTracker( 1, 'Creating test data to work with on bug-tracker', 'github.com/Daemonlord92/Developer-portfoilo-fe','Angular, Ruby on Rails' )
+	bugOne: BugTracker = new BugTracker( 1, 'Creating test data to work with on bug-tracker', 'https://github.com/Daemonlord92/Developer-portfoilo-fe','Angular' )
 
-	bugTwo: BugTracker = new BugTracker( 2, 'working on bootstrap design', 'github.com/Daemonlord92/Developer-portfoilo-fe','Angular, Ruby on Rails' )
+	bugTwo: BugTracker = new BugTracker( 2, 'working on bootstrap design', 'https://github.com/Daemonlord92/Developer-portfoilo-fe','Angular' )
 
-	bugThree: BugTracker = new BugTracker( 3, 'Begin to work on the homepage', 'github.com/Daemonlord92/Developer-portfoilo-fe','Angular, Ruby on Rails' )
+	bugThree: BugTracker = new BugTracker( 3, 'Begin to work on the homepage', 'https://github.com/Daemonlord92/Developer-portfoilo-fe','Angular' )
 
-	bugFour: BugTracker = new BugTracker( 4, 'Setup Project page and testing data', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails' )
+	bugFour: BugTracker = new BugTracker( 4, 'Setup Project page and testing data', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular' )
 
-	bugFive: BugTracker = new BugTracker( 5, 'Setup blog page and testing', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugFive: BugTracker = new BugTracker( 5, 'Setup blog page and testing', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugSix: BugTracker = new BugTracker( 6, 'Setup the contact page, and using the homepage outline to setup about me page', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugSix: BugTracker = new BugTracker( 6, 'Setup the contact page, and using the homepage outline to setup about me page', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugSeven: BugTracker = new BugTracker( 7, 'Fixed a Compile error on the Bug[], changed to BugTracker[]', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugSeven: BugTracker = new BugTracker( 7, 'Fixed a Compile error on the Bug[], changed to BugTracker[]', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugEight: BugTracker = new BugTracker( 8, 'Working on about me Page', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugEight: BugTracker = new BugTracker( 8, 'Working on about me Page', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugNine: BugTracker = new BugTracker( 9, 'Adding a form to bug tracker to add bugs', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugNine: BugTracker = new BugTracker( 9, 'Adding a form to bug tracker to add bugs', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugTen: BugTracker = new BugTracker( 10, 'Create a form for BlogNewComponent and create BlogNewComponent', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
+	bugTen: BugTracker = new BugTracker( 10, 'Create a form for BlogNewComponent and create BlogNewComponent', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
-	bugEleven: BugTracker = new BugTracker( 11, 'Working on Pagination for BugTrackerComponent using jw-angular-pagination', 'github.com/Daemonlord92/Developerportfoilo-fe', 'Angular, Ruby on Rails')
-	bugTwelve: BugTracker = new BugTracker( 12, "Impletementing Pagination for blog post", "github.com/Daemonlord92/Developerportfoilo-fe", 'Angular, Ruby on Rails')
+	bugEleven: BugTracker = new BugTracker( 11, 'Working on Pagination for BugTrackerComponent using jw-angular-pagination', 'https://github.com/Daemonlord92/Developerportfoilo-fe', 'Angular')
 
+	bugTwelve: BugTracker = new BugTracker( 12, "Impletementing Pagination for blog post", "https://github.com/Daemonlord92/Developerportfoilo-fe", 'Angular')
+
+	bugThirdteen: BugTracker = new BugTracker( 13, "Working on the tablet and mobile version of the site!", "https://github.com/Daemonlord92/Developerportfoilo-fe", 'Angular' )
+	bugFourteen: BugTracker = new BugTracker( 13, "Corrected Link bug in bug-tracker!", "https://github.com/Daemonlord92/Developerportfoilo-fe", 'Angular' )
 	
 	bugs: BugTracker[] = [
 	this.bugOne,
@@ -46,6 +49,8 @@ export class BugTrackerComponent implements OnInit {
 	this.bugTen,
 	this.bugEleven,
 	this.bugTwelve,
+	this.bugThirdteen,
+	this.bugFourteen,
 	]
 
   items = this.bugs;
