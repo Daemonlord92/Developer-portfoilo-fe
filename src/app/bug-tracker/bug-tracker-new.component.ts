73f9f64f8cf7bc0f014b6da0da54cc3e292f94a9
Matthew Martin <zhintateo19924559@gmail.com>
2021-02-09 09:03:11 -0600

    Connected the BugTracker new component to the front-end

--- src/app/bug-tracker/bug-tracker-new.component.ts
+++ src/app/bug-tracker/bug-tracker-new.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BugTracker } from './bug-tracker';
 
 @Component({
 	selector: 'app-bug-tracker-new',
 	templateUrl: 'bug-tracker-new.component.html'
 })
-export class BugTrackerNewComponent {	
+export class BugTrackerNewComponent implements OnInit {	
 	bugtracker = new BugTracker;
+
+	loading: boolean = false;
+	items;
+	errorMessage;
+
+	constructor(private client:HttpClient) {}
+
+	public postBugIssue() {
+		this.loading = true;
+		this.errorMessage = "";
+		this.client.post<BugTracker>('http://localhost:4000/api/bugtracker', this.bugtracker)
+		.subscribe( (res) => {
+			console.log(res);
+		},
+		(error) => {
+			this.errorMessage = error;
+			this.loading = false;
+		})
+	}
+
+	ngOnInit() {
+		this.postBugIssue();
+	}
 }
